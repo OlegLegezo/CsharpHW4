2eb8031c57bdb315740764629cd25c55d1d43e3e
OlegLegezo <oleglegezo@yandex.ru>
2022-05-27 09:25:14 +0300

    массив рандомно методом, размер массива задает пользователь

---
+++ ex29/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив целых чисел и выводит их на экран.
+// Размер массива определяется пользователем.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+
+Console.WriteLine("введите размер массива");
+int num = int.Parse(Console.ReadLine());//сразу перевели число в инт
+
+Random rnd = new Random();
+
+//метод создания массива
+int[] initArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(-9, 10);
+        Console.Write(array[i] + " "); //если нужно отобразить
+    }
+    return array;
+}
+//метод создания массива
+
+int[] arr = initArray(num);
