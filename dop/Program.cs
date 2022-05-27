e5e10ecf75a887115027a04b4e78a72a1abae226
OlegLegezo <oleglegezo@yandex.ru>
2022-05-27 10:14:09 +0300

    поиск одинаковых букв в слове(строке)

---
+++ dop/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите функцию, которая принимает слово и возвращает true, если в этом слове есть две одинаковые, стоящие рядом буквы.
+// "ссора" -> true
+// "клюющий" -> true
+// "кодер" -> false
+// "цыпленок"-> false
+
+Console.WriteLine("введите слово");
+var word = Console.ReadLine();
+
+bool DoubleWord = false;
+for (int i = 0; i < word.Length; i++)//word.Lenght - длина строки
+{
+    int previus = i - 1;
+    if (i > 0 && word[i] == word[previus])
+    {
+        DoubleWord = true;
+    }
+}
+
+Console.WriteLine(DoubleWord);
+
+
